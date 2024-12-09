4a3934a1f4741ba725a8f7e73036ec82f13ebbd1
SoRa <sonamraisx007@gmail.com>
2024-12-09 14:40:32 +0545

    ultra_led

--- Cloud&IoT/Workshop6/src/main.cpp
+++ Cloud&IoT/Workshop6/src/main.cpp
@@ -1,25 +1,36 @@
 #include <Arduino.h>
-
 int trigPin = 7;
 int echoPin = 8;
+int ledPin = 13;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.println("Setup Complete!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int distance, duration;
+  int duration, distance;
 
   digitalWrite(trigPin, HIGH);
   delay(10);
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
-  Serial.println("Duration: " + duration);
+
   distance = duration / 58;
-  Serial.println("Distance in CM: " + distance);
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
+
+  if (distance < 20){
+    digitalWrite(ledPin, HIGH);
+  } else if (distance > 20 && distance < 40){
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
 }
