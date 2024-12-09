75a071165dd6d6be2ebd5f2aebf0f2a7742a2bdb
SoRa <sonamraisx007@gmail.com>
2024-12-09 10:03:15 +0545

    class end

--- Cloud&IoT/Workshop6/src/main.cpp
+++ Cloud&IoT/Workshop6/src/main.cpp
@@ -2,27 +2,24 @@
 
 int trigPin = 7;
 int echoPin = 8;
-int ledPin = 13;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  Serial.println("Setup Complete!");
+Serial.begin(9600);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int distance, duration;
+
   digitalWrite(trigPin, HIGH);
   delay(10);
   digitalWrite(trigPin, LOW);
 
-  //finding distance and duration
   duration = pulseIn(echoPin, HIGH);
   Serial.println("Duration: " + duration);
-  distance = (duration / 2) * 29.1;
-  Serial.print("Distance: " + distance);
+  distance = duration / 58;
+  Serial.println("Distance in CM: " + distance);
 }
